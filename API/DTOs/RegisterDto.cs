1a8a94981ca41a6c96e00826ec9467898de56eb5
cath <catherinemleyson@gmail.com>
2023-12-17 23:28:04 +0800

    Adds auth in backend

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    [Required]
+    public bool Gender { get; set; }
+
+}
